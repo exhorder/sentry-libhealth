f5cf1f394fa235a6c8e3145c9a4c9738d2bab53e
Armin Ronacher <armin.ronacher@active-4.com>
2016-11-04 15:48:47 +0100

    Moved producer code over from tervis

--- libtervis/producer.py
+++ libtervis/producer.py
@@ -1,8 +1,48 @@
-from confluent_kafka import Producer
+import json
+import logging
+import functools
+
+from confluent_kafka import Producer as KafkaProducer
 
 from libtervis.utils import merge
 
 
-def make_producer(config):
-    return Producer(merge(config.get('common') or {},
-                          config.get('producer') or {}))
+class Producer(object):
+
+    def __init__(self, config, logger=None):
+        if logger is None:
+            logger = logging.getLogger(__name__)
+        self._producer = KafkaProducer(
+            merge(config.get('common') or {},
+                  config.get('producer') or {}))
+        self.event_count = 0
+
+    def flush(self):
+        return self._producer.flush()
+
+    def produce_event(self, project, event, timestamp=None):
+        produce = functools.partial(
+            self._producer.produce, 'events',
+            json.dumps([project, event]).encode('utf-8'),
+            key=str(project).encode('utf-8'))
+
+        try:
+            produce()
+        except BufferError as e:
+            self.logger.info(
+                'Caught %r, waiting for %s events to be produced',
+                e,
+                len(self.producer),
+            )
+            self.producer.flush()  # wait for buffer to empty
+            self.logger.info('Done waiting, continue to generate events')
+            produce()
+
+        self.event_count += 1
+
+        i = self.event_count
+        if i % 1000 == 0:
+            if timestamp is None:
+                timestamp = time.time()
+            self.logger.info('%s events produced, current timestamp is %s',
+                             i, timestamp)
